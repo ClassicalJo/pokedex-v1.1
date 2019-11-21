3bbc7ccf53f36ea9c96080da1961261cc6500ca6
ClassicalJo <jb.barrientos@yahoo.com>
2019-11-21 17:16:26 +1100

    Updates

---
+++ precache-manifest.d62d353fa68c6aaae9c992c1c2f7ba6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04e3820f33f6cfac2143e7aeb2c46b8",
+    "url": "/pokedex-v1.1/index.html"
+  },
+  {
+    "revision": "38a34964ee799e55b648",
+    "url": "/pokedex-v1.1/static/css/main.63426d72.chunk.css"
+  },
+  {
+    "revision": "8d1a5340935660e09d38",
+    "url": "/pokedex-v1.1/static/js/2.be368584.chunk.js"
+  },
+  {
+    "revision": "38a34964ee799e55b648",
+    "url": "/pokedex-v1.1/static/js/main.e2e5f1e8.chunk.js"
+  },
+  {
+    "revision": "cfad9392ad4f1d38da0f",
+    "url": "/pokedex-v1.1/static/js/runtime-main.b773f8ab.js"
+  }
+]);
